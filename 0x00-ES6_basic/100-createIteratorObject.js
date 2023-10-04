b46ad8f11e7cc92d77c2fb0499fb7cf8ee03272d
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-10-04 08:39:03 +0000

    added new correct file

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,28 +1,10 @@
 export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  return {
-    [Symbol.iterator]: function () {
-      return this;
-    },
-    next: function () {
-      if (currentDepartmentIndex < departments.length) {
-        const employeesInCurrentDepartment = departments[currentDepartmentIndex];
-        if (currentEmployeeIndex < employeesInCurrentDepartment.length) {
-          const employee = employeesInCurrentDepartment[currentEmployeeIndex];
-          currentEmployeeIndex++;
-          return { value: employee, done: false };
-        } else {
-          currentDepartmentIndex++;
-          currentEmployeeIndex = 0;
-          return this.next(); // Move to the next department
-        }
-      } else {
-        return { done: true };
-      }
-    },
-  };
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  return allEmployees;
 }
