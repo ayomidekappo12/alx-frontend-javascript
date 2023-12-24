476daf0bf206fada924d59f607e01967ca94ed03
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-12-24 19:51:37 +0000

    added new files

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,18 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    resolve('Resolved');
+// 0-promise.js
+
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous API call
+    setTimeout(() => {
+      const success = true; // You can modify this based on your API response
+      if (success) {
+        resolve("API response data"); // Resolve with the actual data from the API
+      } else {
+        reject(new Error("API request failed")); // Reject with an error if the API request fails
+      }
+    }, 1000); // Simulating a delay of 1000 milliseconds (1 second)
   });
 }
+
+export default getResponseFromAPI;
+
